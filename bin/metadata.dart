3532c2bdc8f04f7679300a8eb2555943a7b799be
Andira Yunita <andhirayp@gmail.com>
2023-03-10 14:09:39 +0700

    custom annotation metadata

--- bin/metadata.dart
+++ bin/metadata.dart
@@ -1,8 +1,10 @@
 /*
-Metadata is used for adding an information with Annotation using @ character
+Metadata is used for adding an information with Annotation using @ character.
 Example:
   @Deprecated  => constant constructor
   @override
+
+Also, to create custom annotation, we can use a class with constant constructor
 */
 
 class Sample {
@@ -15,3 +17,15 @@ class Sample {
   void doNotUseThis() {}
 }
 
+// Create custom Annotation
+class Todo {
+  final String todo;
+  
+  const Todo(this.todo);
+}
+
+class ApplicationLogic {
+  // Custom annotation using Constant constructor
+  @Todo('Will be implemented in the next feature')
+  void run() {}
+}
